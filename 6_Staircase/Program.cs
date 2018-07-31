2ea6a3fd7f4b8b3f61aaa4f5c1dc451a79be2b0f
DESKTOP-MUEPRUK\Rinat <rrrinat84@gmail.com>
2018-08-01 03:05:33 +0900

    1) 6_staircase 2) 7_minimaxsum in progress

--- 6_Staircase/Program.cs
+++ 6_Staircase/Program.cs
@@ -16,25 +16,42 @@
 
     class Solution
     {
+        /*
+        static void staircase(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {                    
+                    if (j >= n - 1 - i)
+                    {
+                        Console.Write('#');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }          
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+        */
         // Complete the staircase function below.
         static void staircase(int n)
         {
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j <= i; j++)
+                for (int j = 0; j < n; j++)
                 {                    
-                    Console.Write(n - j);
-                    //Console.Write(String.Format("{0, 5}", '#'));
-                    //Console.Write('#');
+                    var s = (j >= n - 1 - i) ? '#' : ' ';
+                    Console.Write(s);
                 }
                 Console.Write("\n");
-                //Console.WriteLine(String.Format("{0,5}", '#'));
             }
-            
             Console.ReadKey();
-
         }
-
+     
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
---
+++ 7_MiniMaxSum/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _7_MiniMaxSum
+{
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Serialization;
+    using System.Text.RegularExpressions;
+    using System.Text;
+    using System;
+
+    class Solution
+    {
+
+        // Complete the miniMaxSum function below.
+        static void miniMaxSum(int[] arr)
+        {
+            int[] result = new int[2];
+
+            int sum = 0;            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            result[0] = sum - arr[arr.Length - 1];
+            result[1] = sum - arr[0];
+
+            Console.WriteLine(String.Join(" ", result));
+
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            miniMaxSum(arr);
+        }
+    }
+}
