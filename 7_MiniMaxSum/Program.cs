0ea0c7c47518b9ac916c48e5b77d8d09576400ce
Rinat <rinat.enikeev@mytona.com>
2018-08-01 13:01:04 +0900

    Mini-Max Sum

--- 7_MiniMaxSum/Program.cs
+++ 7_MiniMaxSum/Program.cs
@@ -18,6 +18,7 @@
     {
 
         // Complete the miniMaxSum function below.
+        /*
         static void miniMaxSum(int[] arr)
         {
             int[] result = new int[2];
@@ -33,6 +34,48 @@
 
             Console.WriteLine(String.Join(" ", result));
 
+            Console.ReadKey();
+        }
+        */
+        // Complete the miniMaxSum function below.
+        static void miniMaxSum(int[] arr)
+        {
+            int[] result = new int[2];
+
+            int[] calculation = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+
+                    calculation[i] += arr[j];
+                }
+            }
+
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < calculation.Length; i++)
+            {
+                if (calculation[i] < calculation[min])
+                {
+                    min = i;
+                }
+
+                if (calculation[i] > calculation[max])
+                {
+                    max = i;
+                }
+            }
+
+            result[0] = calculation[min];
+            result[1] = calculation[max];
+
+            Console.WriteLine(String.Join(" ", result));
             Console.ReadKey();
         }
 
